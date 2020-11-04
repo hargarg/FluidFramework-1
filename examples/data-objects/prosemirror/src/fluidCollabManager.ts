ad3da9117ae0b6d0b3549091e6e5b9c1216ba53b
unknown <prerna.jain@microsoft.com>
2020-11-04 19:51:38 +0530

    updated content style

--- examples/data-objects/prosemirror/src/fluidCollabManager.ts
+++ examples/data-objects/prosemirror/src/fluidCollabManager.ts
@@ -316,10 +316,10 @@ export class FluidCollabManager extends EventEmitter implements IRichTextEditor 
 
         const textContent = proseMirrorNode.content.firstChild?.text?.toString();
 
-        this.emit("selection", {textContent, cb: (content: string) => {this.updateTooltipContent(content, view, from, to)}})
+        this.emit("selection", {textContent, cb: (content: Array<Object>) => {this.updateTooltipContent(content, view, from, to)}})
     }
 
-    public updateTooltipContent(tooltipContent: string, view: EditorView, from: number, to: number) {
+    public updateTooltipContent(content: Array<Object>, view: EditorView, from: number, to: number) {
         this.tooltip.style.display = ""
         // These are in screen coordinates
         let start = view.coordsAtPos(from), end = view.coordsAtPos(to)
@@ -331,7 +331,13 @@ export class FluidCollabManager extends EventEmitter implements IRichTextEditor 
         this.tooltip.style.left = (left - box.left) + "px"
         this.tooltip.style.bottom = (box.bottom - start.top) + "px"
 
-        this.tooltip.textContent = tooltipContent;
+        let tooltipContent: string = ``;
+        content.forEach(element => {
+            const key = Object.keys(element)[0];
+            tooltipContent += `<div><b>${key}</b> : ${element[key]}</div>`;
+        });
+
+        this.tooltip.innerHTML = tooltipContent;
     }
 
     public destroySelection() { this.tooltip.remove(); }
